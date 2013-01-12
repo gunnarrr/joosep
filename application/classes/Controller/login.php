8435d7a9d346c0a6caf3710b7a737b938d5324a1
Kaido Muttik <kaits90@gmail.com>
2013-01-12 13:32:44 +0200

    authenticating, login page, mysql user joosep

--- application/classes/Controller/login.php
+++ application/classes/Controller/login.php
@@ -6,13 +6,13 @@
  * Time: 12:44 PM
  * To change this template use File | Settings | File Templates.
  */
-class Controller_Welcome extends Controller_Login {
+class Controller_Login extends Controller_Template {
 
 	public function action_index()
 	{
 		//$this->response->body('Identify yourself.');
 
-	public function action_login()
+
 	}
 
 }//Login page
