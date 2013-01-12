fb0aa5624455db3d162d3fa219cb5604a74b7f5c
Kaido Muttik <kaits90@gmail.com>
2013-01-12 13:40:00 +0200

    authenticating, login page, mysql user joosep

--- application/classes/Controller/Auth.php
+++ application/classes/Controller/Auth.php
@@ -1,4 +1,4 @@
-<?php
+<?php defined('SYSPATH') or die('No direct script access.');
 /**
  * Created by JetBrains PhpStorm.
  * User: lubuntu
@@ -6,11 +6,36 @@
  * Time: 12:45 PM
  * To change this template use File | Settings | File Templates.
  */
-class Controller_Auth extends Controller_Template {
+
+
+
+class Controller_Auth extends Controller_Template
+{
+	public $template = 'templates/public';
 
 	public function action_index()
 	{
-		$this->response->body('hello, world!');
+		if ($this->request->post('username')) {
+			$username = $this->request->post('username');
+			$password = $this->request->post('password');
+			//echo Auth::instance()->hash('demo');
+			$is_logged_in = Auth::instance()->login($username, $password, true);
+
+			if ($is_logged_in) {
+				Notify::success('Sisse logitud');
+				$this->redirect('Welcome');
+			} else {
+				Notify::error('Sisselogimine ebaõnnestus');
+			}
+		} else {
+
+		}
 	}
 
-}
+	public function action_logout()
+	{
+		Auth::instance()->logout();
+		$this->redirect('auth');
+	}
+
+} // End Welcome
